ed11b39ba35a4d0421f653bd1444dd207cd3c34f
kitimark <markdog_@hotmail.com>
2025-11-17 03:18:39 +0700

    hw2, q2

--- hw2_kiti_mark.hs
+++ hw2_kiti_mark.hs
@@ -63,3 +63,71 @@ roseAreaWithRecurison r
   where
     epsilon = 0.1
     petalArea = area $ petal r
+
+-- Question 2; implement some data structure (prefer avl tree)
+data AVLTree a
+  = EmptyAVLTree
+  | Node
+      { value :: a,
+        left :: AVLTree a,
+        right :: AVLTree a
+      }
+  deriving (Show, Eq)
+
+instance Foldable AVLTree where
+  foldr :: (a -> b -> b) -> b -> AVLTree a -> b
+  foldr _ e EmptyAVLTree = e
+  foldr f e (Node x lt rt) = foldr f (f x (foldr f e rt)) lt
+
+singleAVLNode :: (Ord a) => a -> AVLTree a
+singleAVLNode a = Node a EmptyAVLTree EmptyAVLTree
+
+insertAVLNode :: (Ord a) => AVLTree a -> a -> AVLTree a
+insertAVLNode EmptyAVLTree a = singleAVLNode a
+insertAVLNode (Node v lt rt) a
+  | a == v = rebalanceTree $ Node v lt rt
+  | a < v = rebalanceTree $ Node v (insertAVLNode lt a) rt
+  | a > v = rebalanceTree $ Node v lt (insertAVLNode rt a)
+
+rebalanceTree :: (Ord a) => AVLTree a -> AVLTree a
+rebalanceTree a
+  | factor == -2 = rebalanceTreeLeft a
+  | factor == 2 = rebalanceTreeRight a
+  | otherwise = a
+  where
+    factor = balanceFactor a
+
+rebalanceTreeLeft :: (Ord a) => AVLTree a -> AVLTree a
+rebalanceTreeLeft a
+  | factor == -1 = rotateLeftLeft a
+  | factor == 1 = rotateLeftRight a
+  where
+    factor = balanceFactor (left a)
+
+rotateLeftLeft :: AVLTree a -> AVLTree a
+rotateLeftLeft (Node v (Node lv llt lrt) rt) = Node lv llt (Node v lrt rt)
+
+rotateLeftRight :: AVLTree a -> AVLTree a
+rotateLeftRight (Node v (Node lv llt (Node lrv lrlt lrrt)) rt) =
+  Node lrv (Node lv llt lrlt) (Node v lrrt rt)
+
+rebalanceTreeRight :: (Ord a) => AVLTree a -> AVLTree a
+rebalanceTreeRight a
+  | factor == 1 = rotateRightRight a
+  | factor == -1 = rotateRightLeft a
+  where
+    factor = balanceFactor (right a)
+
+rotateRightRight :: AVLTree a -> AVLTree a
+rotateRightRight (Node v lt (Node rv rlt rrt)) = Node rv (Node v lt rlt) rrt
+
+rotateRightLeft :: AVLTree a -> AVLTree a
+rotateRightLeft (Node v lt (Node rv (Node rlv rllt rlrt) rr)) =
+  Node rlv (Node v lt rllt) (Node rv rlrt rr)
+
+balanceFactor :: AVLTree a -> Int
+balanceFactor (Node _ lt rt) = -heightTree lt + heightTree rt
+
+heightTree :: AVLTree a -> Int
+heightTree EmptyAVLTree = 0
+heightTree (Node _ lt rt) = 1 + max (heightTree lt) (heightTree rt)
--- hw2_kiti_mark_test.hs
+++ hw2_kiti_mark_test.hs
@@ -213,6 +213,225 @@ roseRecursiveAreaTests =
          in mkApproxTest "sums halved petal areas until threshold" 1e-12 expected (roseAreaWithRecurison rose)
       ]
 
+singleAVLNodeTests :: Test
+singleAVLNodeTests =
+  TestLabel "singleAVLNode" $
+    TestList
+      [ let expected = Node 42 EmptyAVLTree EmptyAVLTree
+         in mkTest "wraps value with empty children" expected (singleAVLNode 42),
+        let actual = singleAVLNode (7 :: Int)
+            expected = Node (7 :: Int) EmptyAVLTree EmptyAVLTree
+         in mkTest "matches manual node construction" expected actual
+      ]
+
+insertAVLNodeTests :: Test
+insertAVLNodeTests =
+  TestLabel "insertAVLNode" $
+    TestList
+      [ let expected = singleAVLNode 10
+         in mkTest "inserts into empty tree" expected (insertAVLNode EmptyAVLTree 10),
+        let base = singleAVLNode (10 :: Int)
+            expected = Node 10 (singleAVLNode 5) EmptyAVLTree
+         in mkTest "places smaller values on the left" expected (insertAVLNode base 5),
+        let base = insertAVLNode (singleAVLNode (10 :: Int)) 5
+            expected = Node 10 (singleAVLNode 5) (singleAVLNode 15)
+         in mkTest "places larger values on the right" expected (insertAVLNode base 15),
+        let base = Node 3 (Node 2 EmptyAVLTree EmptyAVLTree) EmptyAVLTree
+            expected = Node 2 (Node 1 EmptyAVLTree EmptyAVLTree) (Node 3 EmptyAVLTree EmptyAVLTree)
+         in mkTest "left-left rebalance" expected (insertAVLNode base 1),
+        let base = Node 3 (Node 1 EmptyAVLTree EmptyAVLTree) EmptyAVLTree
+            expected = Node 2 (Node 1 EmptyAVLTree EmptyAVLTree) (Node 3 EmptyAVLTree EmptyAVLTree)
+         in mkTest "left-right rebalance" expected (insertAVLNode base 2),
+        let base = Node 1 EmptyAVLTree (Node 2 EmptyAVLTree EmptyAVLTree)
+            expected = Node 2 (Node 1 EmptyAVLTree EmptyAVLTree) (Node 3 EmptyAVLTree EmptyAVLTree)
+         in mkTest "right-right rebalance" expected (insertAVLNode base 3),
+        let base = Node 1 EmptyAVLTree (Node 3 EmptyAVLTree EmptyAVLTree)
+            expected = Node 2 (Node 1 EmptyAVLTree EmptyAVLTree) (Node 3 EmptyAVLTree EmptyAVLTree)
+         in mkTest "right-right rebalance" expected (insertAVLNode base 2)
+      ]
+
+avlTreeWalkthroughTests :: Test
+avlTreeWalkthroughTests =
+  TestLabel "AVLTree" $
+    TestList
+      [ mkTest "step1 - insert 1" expected1 step1,
+        mkTest "step1 - length" 1 (length step1),
+        mkTest "step2 - insert 2" expected2 step2,
+        mkTest "step2 - length" 2 (length step2),
+        mkTest "step3 - insert 3" expected3 step3,
+        mkTest "step3 - length" 3 (length step3),
+        mkTest "step4 - insert 5" expected4 step4,
+        mkTest "step4 - length" 4 (length step4),
+        mkTest "step5 - insert -1" expected5 step5,
+        mkTest "step5 - length" 5 (length step5),
+        mkTest "step6 - insert 6" expected6 step6,
+        mkTest "step6 - length" 6 (length step6),
+        mkTest "step7 - insert 8" expected7 step7,
+        mkTest "step7 - length" 7 (length step7),
+        mkTest "step8 - insert 10" expected8 step8,
+        mkTest "step8 - length" 8 (length step8),
+        mkTest "step9 - insert 11" expected9 step9,
+        mkTest "step9 - length" 9 (length step9),
+        mkTest "step10 - insert 4" expected10 step10,
+        mkTest "step10 - length" 10 (length step10),
+        mkTest "step11 - insert 7" expected11 step11,
+        mkTest "step11 - length" 11 (length step11)
+      ]
+  where
+    step0 = EmptyAVLTree :: AVLTree Int
+    step1 = insertAVLNode step0 1
+    step2 = insertAVLNode step1 2
+    step3 = insertAVLNode step2 3
+    step4 = insertAVLNode step3 5
+    step5 = insertAVLNode step4 (-1)
+    step6 = insertAVLNode step5 6
+    step7 = insertAVLNode step6 8
+    step8 = insertAVLNode step7 10
+    step9 = insertAVLNode step8 11
+    step10 = insertAVLNode step9 4
+    step11 = insertAVLNode step10 7
+
+    expected1 = Node 1 EmptyAVLTree EmptyAVLTree
+    expected2 = Node 1 EmptyAVLTree (singleAVLNode 2)
+    expected3 = Node 2 (singleAVLNode 1) (singleAVLNode 3)
+    expected4 = Node 2 (singleAVLNode 1) (Node 3 EmptyAVLTree (singleAVLNode 5))
+    expected5 =
+      Node
+        2
+        (Node 1 (singleAVLNode (-1)) EmptyAVLTree)
+        (Node 3 EmptyAVLTree (singleAVLNode 5))
+    expected6 =
+      Node
+        2
+        (Node 1 (singleAVLNode (-1)) EmptyAVLTree)
+        (Node 5 (singleAVLNode 3) (singleAVLNode 6))
+    expected7 =
+      Node
+        2
+        (Node 1 (singleAVLNode (-1)) EmptyAVLTree)
+        (Node 5 (singleAVLNode 3) (Node 6 EmptyAVLTree (singleAVLNode 8)))
+    expected8 =
+      Node
+        2
+        (Node 1 (singleAVLNode (-1)) EmptyAVLTree)
+        (Node 5 (singleAVLNode 3) (Node 8 (singleAVLNode 6) (singleAVLNode 10)))
+    expected9 =
+      Node
+        2
+        (Node 1 (singleAVLNode (-1)) EmptyAVLTree)
+        ( Node
+            8
+            (Node 5 (singleAVLNode 3) (singleAVLNode 6))
+            (Node 10 EmptyAVLTree (singleAVLNode 11))
+        )
+    expected10 =
+      Node
+        5
+        ( Node
+            2
+            (Node 1 (singleAVLNode (-1)) EmptyAVLTree)
+            ( Node
+                3
+                EmptyAVLTree
+                (singleAVLNode 4)
+            )
+        )
+        ( Node
+            8
+            (singleAVLNode 6)
+            ( Node
+                10
+                EmptyAVLTree
+                (singleAVLNode 11)
+            )
+        )
+    expected11 =
+      Node
+        5
+        ( Node
+            2
+            (Node 1 (singleAVLNode (-1)) EmptyAVLTree)
+            ( Node
+                3
+                EmptyAVLTree
+                (singleAVLNode 4)
+            )
+        )
+        ( Node
+            8
+            ( Node
+                6
+                EmptyAVLTree
+                (singleAVLNode 7)
+            )
+            ( Node
+                10
+                EmptyAVLTree
+                (singleAVLNode 11)
+            )
+        )
+
+balanceFactorTests :: Test
+balanceFactorTests =
+  TestLabel "balanceFactor" $
+    TestList
+      [ mkTest "leaf nodes are balanced" 0 (balanceFactor (singleAVLNode (1 :: Int))),
+        let base = Node 8 EmptyAVLTree EmptyAVLTree
+         in mkTest "symmetric three-node tree has 0 balance - 1st" 0 (balanceFactor base),
+        let base =
+              Node
+                8
+                (Node 4 EmptyAVLTree EmptyAVLTree)
+                (Node 9 EmptyAVLTree EmptyAVLTree)
+         in mkTest "symmetric three-node tree has 0 balance - 2rd" 0 (balanceFactor base),
+        let base = Node 8 (Node 4 EmptyAVLTree EmptyAVLTree) EmptyAVLTree
+         in mkTest "unbalance left-node tree has -1 balance" (-1) (balanceFactor base),
+        let base =
+              Node
+                8
+                EmptyAVLTree
+                (Node 9 EmptyAVLTree EmptyAVLTree)
+         in mkTest "unbalance right-node tree has 1 balance" 1 (balanceFactor base),
+        let base =
+              Node
+                8
+                ( Node
+                    4
+                    (Node 2 EmptyAVLTree EmptyAVLTree)
+                    EmptyAVLTree
+                )
+                EmptyAVLTree
+         in mkTest "unbalance left-node tree has -2 balance" (-2) (balanceFactor base),
+        let base =
+              Node
+                8
+                EmptyAVLTree
+                ( Node
+                    10
+                    EmptyAVLTree
+                    (Node 12 EmptyAVLTree EmptyAVLTree)
+                )
+         in mkTest "unbalance left-node tree has 2 balance" 2 (balanceFactor base)
+      ]
+
+heightTreeTests :: Test
+heightTreeTests =
+  TestLabel "heightTree" $
+    TestList
+      [ mkTest "empty tree has height zero" 0 (heightTree (EmptyAVLTree :: AVLTree Int)),
+        mkTest "single node tree has height one" 1 (heightTree (singleAVLNode (42 :: Int))),
+        let tree =
+              Node
+                (10 :: Int)
+                ( Node
+                    5
+                    (singleAVLNode 2)
+                    (singleAVLNode 7)
+                )
+                (singleAVLNode 15)
+         in mkTest "height tracks deepest branch" 3 (heightTree tree)
+      ]
+
 allTests :: Test
 allTests =
   TestList
@@ -226,7 +445,12 @@ allTests =
       petalInnerTests,
       roseAreaTests,
       roseInnerTests,
-      roseRecursiveAreaTests
+      roseRecursiveAreaTests,
+      singleAVLNodeTests,
+      insertAVLNodeTests,
+      avlTreeWalkthroughTests,
+      balanceFactorTests,
+      heightTreeTests
     ]
 
 main :: IO ()
